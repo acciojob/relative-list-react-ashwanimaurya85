9dae32e23ad40ceacc4fbac62d705c787b64395d
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 13:26:10 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+const App = () => {
+  // List of relatives
+  const relatives = [
+    'Relative 1',
+    'Relative 2',
+    'Relative 3',
+    'Relative 4',
+    'Relative 5',
+  ];
+
+  return (
+    <div>
+      <h2>Diwali Shopping List</h2>
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
+
